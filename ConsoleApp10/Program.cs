2ef8a1729aa9723294649f4cbb50ed837034d3e7
esraa <em5938941@gmail.com>
2025-08-05 18:40:12 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+
+            students.Add("samer", 101);
+            students.Add("Esraa", 102);
+            students.Add("Omar", 103);
+
+            Console.WriteLine("Esraa's seat number is: " + students["Esraa"]);
+
+            if (students.ContainsKey("Ali"))
+            {
+                Console.WriteLine("Ali is in the list.");
+            }
+
+            students["Ali"] = 110;
+
+            students.Remove("Omar");
+
+            Console.WriteLine("\nAll students:");
+            foreach (var pair in students)
+            {
+                Console.WriteLine($"Name: {pair.Key}, Seat: {pair.Value}");
+            }
+
+        }
+    }
+}
